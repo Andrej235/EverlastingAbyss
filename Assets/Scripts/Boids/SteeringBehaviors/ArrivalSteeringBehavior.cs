d2f1f8e83dd75f98776af76b391e03e2737944ac
Andrej Nenadić <andrejnana445@gmail.com>
2024-09-20 15:13:06 +0200

    Add Flee and Arrival steering behaviors

--- Assets/Scripts/Boids/SteeringBehaviors/ArrivalSteeringBehavior.cs
+++ Assets/Scripts/Boids/SteeringBehaviors/ArrivalSteeringBehavior.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
-public class SeekSteeringBehavior : MonoBehaviour
+public class ArrivalSteeringBehavior : MonoBehaviour
 {
     [SerializeField] private Transform targetTransform;
     [SerializeField] private float maxVelocity = 3;
     [SerializeField] private float maxForce = 15;
     [SerializeField] private float mass = 15;
+    [SerializeField] private float slowingRadius = 3;
 
     private new Rigidbody rigidbody;
     private void Start() => rigidbody = GetComponent<Rigidbody>();
@@ -16,11 +17,13 @@ public class SeekSteeringBehavior : MonoBehaviour
         Vector3 velocity = rigidbody.velocity;
         Vector3 position = transform.position;
 
-        Vector3 desiredVelocity = Vector3.Normalize(target - position) * maxVelocity;
-        Vector3 steering = desiredVelocity - velocity;
+        Vector3 desiredVelocity = target - position;
+        float distance = desiredVelocity.magnitude;
+        desiredVelocity = distance < slowingRadius
+            ? Vector3.Normalize(desiredVelocity) * maxVelocity * (distance / slowingRadius)
+            : Vector3.Normalize(desiredVelocity) * maxVelocity;
 
-        steering = Vector3.ClampMagnitude(steering, maxForce);
-        steering /= mass;
+        Vector3 steering = desiredVelocity - velocity;
 
         velocity = Vector3.ClampMagnitude(velocity + steering, maxVelocity);
         rigidbody.velocity = velocity;
--- Assets/Scripts/Boids/SteeringBehaviors/FleeSteeringBehavior.cs
+++ Assets/Scripts/Boids/SteeringBehaviors/FleeSteeringBehavior.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
-public class SeekSteeringBehavior : MonoBehaviour
+public class FleeSteeringBehavior : MonoBehaviour
 {
     [SerializeField] private Transform targetTransform;
     [SerializeField] private float maxVelocity = 3;
     [SerializeField] private float maxForce = 15;
     [SerializeField] private float mass = 15;
+    [SerializeField] private float minFleeDistance = 10;
 
     private new Rigidbody rigidbody;
     private void Start() => rigidbody = GetComponent<Rigidbody>();
@@ -16,7 +17,10 @@ public class SeekSteeringBehavior : MonoBehaviour
         Vector3 velocity = rigidbody.velocity;
         Vector3 position = transform.position;
 
-        Vector3 desiredVelocity = Vector3.Normalize(target - position) * maxVelocity;
+        if (Vector3.Distance(position, target) > minFleeDistance)
+            return;
+
+        Vector3 desiredVelocity = Vector3.Normalize(position - target) * maxVelocity; //For seeking, the only difference is (target - position) not (position - target)
         Vector3 steering = desiredVelocity - velocity;
 
         steering = Vector3.ClampMagnitude(steering, maxForce);

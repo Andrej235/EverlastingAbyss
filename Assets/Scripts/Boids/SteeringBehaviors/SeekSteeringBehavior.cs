e66229085789de509739bcce25a613020aa20399
Andrej Nenadić <andrejnana445@gmail.com>
2024-09-20 13:50:16 +0200

    Add SeekSteeringBehavior

---
+++ Assets/Scripts/Boids/SteeringBehaviors/SeekSteeringBehavior.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class SeekSteeringBehavior : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    private new Rigidbody rigidbody;
+    [SerializeField] private float maxVelocity = 3;
+    [SerializeField] private float maxForce = 15;
+    [SerializeField] private float mass = 15;
+
+    private void Start() => rigidbody = GetComponent<Rigidbody>();
+
+    private void Update()
+    {
+        Vector3 target = targetTransform.position;
+        Vector3 velocity = rigidbody.velocity;
+        Vector3 position = transform.position;
+
+        Vector3 desiredVelocity = Vector3.Normalize(target - position) * maxVelocity;
+        Vector3 steering = desiredVelocity - velocity;
+
+        steering = Vector3.ClampMagnitude(steering, maxForce);
+        steering /= mass;
+
+        velocity = Vector3.ClampMagnitude(velocity + steering, maxVelocity);
+        rigidbody.velocity = velocity;
+
+        Debug.DrawRay(transform.position, velocity.normalized * 2, Color.green);
+        Debug.DrawRay(transform.position, desiredVelocity.normalized * 2, Color.magenta);
+    }
+}
